15ab1be92208f075aa02d9d09d12426ca75ed144
RaphaelNY <1278173058@qq.com>
2024-03-19 23:33:04 +0800

    fix function to the config.

--- src/config_database.rs
+++ src/config_database.rs
@@ -1,4 +1,5 @@
-use anyhow::Context;
+use std::collections::HashMap;
+use std::io;
 use anyhow::Error;
 
 #[derive(Debug)]
@@ -10,20 +11,39 @@ pub struct DatabaseConfig {
 
 impl DatabaseConfig{
     pub fn from_file(filename: &str) -> Result<Self, Error> {
-        let config = std::fs::read_to_string(filename)
-            .with_context(|| format!("Error reading file: {}", filename))?;
+        let config_str = std::fs::read_to_string(filename)?;
+        let config: HashMap<String, toml::Value> = toml::from_str(&config_str)?;
 
-        let config: toml::Value = toml::from_str(&config)
-            .with_context(|| format!("Error parsing file: {}", filename))?;
+        // 提取 [database] 表格中的字段
+        let database_config = match config.get("database") {
+            Some(database_table) => {
+                let database_table = database_table
+                    .as_table()
+                    .ok_or(io::Error::new(io::ErrorKind::Other, "Expected a table for [database]"))?;
 
-        let host_port = config["host"].as_str().ok_or_else(|| Error::msg("host_port not found"))?.to_string();
-        let username = config["username"].as_str().ok_or_else(|| Error::msg("username not found"))?.to_string();
-        let password = config["password"].as_str().ok_or_else(|| Error::msg("password not found"))?.to_string();
+                let host_port = database_table
+                    .get("host")
+                    .and_then(|v| v.as_str())
+                    .ok_or(io::Error::new(io::ErrorKind::Other, "host not found or not a string"))?
+                    .to_string();
 
-        Ok(DatabaseConfig {
-            host_port,
-            username,
-            password,
-        })
+                let username = database_table
+                    .get("username")
+                    .and_then(|v| v.as_str())
+                    .ok_or(io::Error::new(io::ErrorKind::Other, "username not found or not a string"))?
+                    .to_string();
+
+                let password = database_table
+                    .get("password")
+                    .and_then(|v| v.as_str())
+                    .ok_or(io::Error::new(io::ErrorKind::Other, "password not found or not a string"))?
+                    .to_string();
+
+            DatabaseConfig { host_port, username, password }
+        }
+            None => return Err(io::Error::new(io::ErrorKind::NotFound, "No [database] table found in config").into()),
+        };
+
+        Ok(database_config)
     }
 }
